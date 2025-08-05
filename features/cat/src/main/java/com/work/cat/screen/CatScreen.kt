611f2c57e31bc1bcf51ac623ec528d2544dfe4d1
kabigon <jakrapanc@gmail.com>
2025-08-05 18:00:06 +0700

    fix: ksp lib version

---
+++ features/cat/src/main/java/com/work/cat/screen/CatScreen.kt
@@ -0,0 +1,15 @@
+package com.work.cat.screen
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+
+@Composable
+fun CatScreen() {
+
+}
+
+@Preview
+@Composable
+fun PreviewCatScreen() {
+
+}
