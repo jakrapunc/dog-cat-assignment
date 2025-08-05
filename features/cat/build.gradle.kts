fc73a35caa8bb664dfea4958d258aa3e02334f26
kabigon <jakrapanc@gmail.com>
2025-08-05 16:58:26 +0700

    chore: init cat feature

---
+++ features/cat/build.gradle.kts
@@ -0,0 +1,43 @@
+plugins {
+    alias(libs.plugins.android.library)
+    alias(libs.plugins.kotlin.android)
+}
+
+android {
+    namespace = "com.work.cat"
+    compileSdk = libs.versions.compileSdkVersion.get().toInt()
+
+    defaultConfig {
+        minSdk = libs.versions.minSdkVersion.get().toInt()
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        consumerProguardFiles("consumer-rules.pro")
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+    kotlin {
+        jvmToolchain(11)
+    }
+}
+
+dependencies {
+    implementation(project(":core:base"))
+    implementation(project(":core:network"))
+    implementation(project(":shares:cat-service"))
+
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.junit)
+    androidTestImplementation(libs.androidx.espresso.core)
+}
