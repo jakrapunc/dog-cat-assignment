f8ea9a3d53b5b269211bac142e1e76ebadc25736
kabigon <jakrapanc@gmail.com>
2025-08-06 04:06:15 +0700

    feat: implement dog screen view model

---
+++ features/dog/src/main/java/com/work/dog/screen/DogScreen.kt
@@ -0,0 +1,15 @@
+package com.work.dog.screen
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+
+@Composable
+fun DogScreen() {
+
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DogScreenPreview() {
+    DogScreen()
+}
